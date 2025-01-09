66532a57508872437272c30172c1957878a31445
Nguyen.Anhduc <Nguyen.Anhduc@qualysoft-coe.com>
2025-01-09 16:54:24 +0700

    routing and add reservation form

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-reservation-form',
   standalone: false,
-  
+
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
+  reservationForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {
+  }
+  
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 }
